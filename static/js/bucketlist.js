770e23aa4708b38cac70a049cf06a49d908ec2ef
Tiffany Allen <tiffallen723@gmail.com>
2016-10-27 18:06:45 -0700

    added popup button for adding a new event

--- static/js/bucketlist.js
+++ static/js/bucketlist.js
@@ -1,29 +1,10 @@
 'use strict';
 
-// Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	initializePage();
-})
-
-/*
- * Function that is called when the document is ready.
- */
-function initializePage() {
-	$("#search").click(function(e) {
-		$('.name value').text("Searching");
-	});
-
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
-	$("main").click(projectClick);
-}
-
-function	projectClick(e)	{
-	console.log("Project clicked");
-	//	Cancel	the	default	action,	which	prevents	the	page	from	reloading
-	e.preventDefault();
-	//	In	an	event	listener,	$(this)	is	the	element	that	fired	the	event
-	var	projectTitle	=	$(this).find("p").text();
-	var	jumbotronHeader	=	$(".jumbotron h1");
-	jumbotronHeader.text(projectTitle);	
-}
+function popup(mylink, windowname) { 
+    if (! window.focus)return true;
+    var href;
+    if (typeof(mylink) == 'string') href=mylink;
+    else href=mylink.href; 
+    window.open(href, windowname, 'width=400,height=200,scrollbars=yes'); 
+    return false; 
+ }
