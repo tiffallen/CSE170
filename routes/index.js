6cb301b230dbf2711b2092a60a0ea9ac3f9935db
Stephanie Villalobos <sgvillal@ucsd.edu>
2016-11-02 16:52:22 -0700

    server up, unconnected memories and home pages

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,20 @@ router.get('/bucketlist', function(req, res, next) {
 	console.log('request was made: ' + req.url);
 	res.sendFile(__dirname + '/Bucketlist.html');
 	//res.render('layout');
+});
+
+router.get('/home', function(req, res, next) {
+
+	console.log('request was made: ' + req.url);
+	res.sendFile(__dirname + '/home.html');
+	//res.render('layout');
+});
+
+router.get('/memories', function(req, res, next) {
+
+	console.log('request was made: ' + req.url);
+	res.sendFile(__dirname + '/memories.html');
+	//res.render('layout');
 
 });
 
