87e34d3611ac17675ea09746a60c9679e15750ff
Stephanie Villalobos <sgvillal@ucsd.edu>
2016-11-02 14:52:07 -0700

    Jade: added stylesheets to public folder

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Australia' });
 });
 
 router.get('/users', function(req, res, next) {
