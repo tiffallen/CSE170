08412910c0f6e51f0d9226eeac09d15f1863c4c5
Marie Schneider <mschneid@ucsd.edu>
2016-11-13 19:28:35 -0800

    Took out some unesseary code/make code more uniform

--- routes/addEvent.js
+++ routes/addEvent.js
@@ -1,14 +1,13 @@
 var data = require("../bucketdata.json");
 
 exports.addEvent = function(req, res) {
-	var tripname = req.query.tripname; 
+	var adventurename = req.query.adventurename; 
 	var category = req.query.category; 
-	var location = req.query.locationideas;
-	var img = "http://lorempixel.com/400/400/people";
-	var newEvent = {"adventureName": tripname, "category": category, "locationideas": location, "adventurepics": img};
+	var locationideas = req.query.locationideas;
+	var adventurepics = "http://lorempixel.com/400/400/people";
+	var newEvent = {"adventurename": adventurename, "category": category, "locationideas": locationideas, "adventurepics": adventurepics};
 	data.mybucketlist.push(newEvent);
 	console.log(newEvent);
-	console.log("yay, addEvent just ran!");
 	return res.redirect("/bucketlist");
 }
 
