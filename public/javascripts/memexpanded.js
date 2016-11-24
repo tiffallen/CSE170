801cafb026ddbb4a48f0ef22f9c59c3fa6ae0c4d
Stephanie Villalobos <sgvillal@ucsd.edu>
2016-11-24 06:01:19 -0800

    layout fixes

--- public/javascripts/memexpanded.js
+++ public/javascripts/memexpanded.js
@@ -13,6 +13,6 @@ exports.view1 = function (req, res){
 	buttonID1 = req.query.buttonID;
 	console.log('Memory Expand: ' + req.query['buttonID1']);
 	console.log('data: ' + data['mymemories']);
-	res.render('memoriesexpand1', data.mymemories[buttonID1]); 
+	res.render('memoriesexpanded1', data.mymemories[buttonID1]); 
 	// will only send the first element of the array "myhomepage"
 }
