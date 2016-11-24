82d34baa2ef37d162714b3d9f12b565952f09faf
Marie Schneider <mschneid@ucsd.edu>
2016-11-23 22:12:11 -0800

    Added AB partial

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,13 +1,21 @@
 var data = require('../../home.json');
 
 exports.view = function (req, res){
+	//myhomepage["grid"] = false;
 	console.log(data);
 	res.render('home', data);
 }
 
 exports.view1 = function (req, res){
+	//myhomepage["grid"] = true;
 	console.log(data);
 	res.render('home1', data);
 }
 
+/*function initializePage() {
+	// your code here
+	$('.btn').click(function() {
+		ga("send", "event", "choose", "click");
+	});
+}*/
 
